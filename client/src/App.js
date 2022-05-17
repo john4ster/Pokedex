4a27c9bf97d7f9fd12fb7234445f70cda16e65b2
john4ster <johnforsterbusiness@gmail.com>
2022-05-17 11:56:58 -0400

    update setGen

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,20 @@ function App() {
 
   const [gen, setGen] = useState(1);
 
-  function changeGeneration(gen) {
-    setGen(gen);
-  }
-
   return (
     <div className="App">
       <div className='topBar'>
         <h1 className='title'>Pokedex</h1>
         <h3 className='generationTitle'>Select Generation:</h3>
         <div className='generationButtons'>
-          <button onClick={e => changeGeneration(1)}>I</button>
-          <button onClick={e => changeGeneration(2)}>II</button>
-          <button onClick={e => changeGeneration(3)}>III</button>
-          <button onClick={e => changeGeneration(4)}>IV</button>
-          <button onClick={e => changeGeneration(5)}>V</button>
-          <button onClick={e => changeGeneration(6)}>VI</button>
-          <button onClick={e => changeGeneration(7)}>VII</button>
-          <button onClick={e => changeGeneration(8)}>VIII</button>
+          <button onClick={e => setGen(1)}>I</button>
+          <button onClick={e => setGen(2)}>II</button>
+          <button onClick={e => setGen(3)}>III</button>
+          <button onClick={e => setGen(4)}>IV</button>
+          <button onClick={e => setGen(5)}>V</button>
+          <button onClick={e => setGen(6)}>VI</button>
+          <button onClick={e => setGen(7)}>VII</button>
+          <button onClick={e => setGen(8)}>VIII</button>
         </div>
       </div>
       <div className='list'>
